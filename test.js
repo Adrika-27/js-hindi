39dab1c6aea343d514868d708671af36936450a7
Adrika-27 <adrikasaloni.it2023@nsec.ac.in>
2026-02-05 17:28:03 +0530

    data types

--- test.js
+++ test.js
@@ -1,1 +1,29 @@
-console.log("adrika")
+
+const accountId=1090022317
+let accountEmail= "saloni@gmail.com"
+var accPassword= "Saloni@123"
+accCity= "Pune"
+let accstate;
+//accountId=2 //not allowed to change const value
+
+console.log(accountId);
+console.table({accountId,accountEmail,accPassword,accCity})
+
+/*data types
+number
+string
+boolean
+undefined
+null=> standalone value
+symbol
+bigint
+object
+
+*/
+
+console.log(typeof accountId);
+console.log(typeof accountEmail);
+console.log(typeof accPassword);
+console.log(typeof accCity);
+console.log(typeof accstate);
+console.log(typeof null); //object Null is standalone value but its type is object beacause of JS bug 
